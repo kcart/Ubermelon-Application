41f9bdd66f51e0370b55cd6cad2b4b9d36b1d74c
Hackbright Student <info@hackbrightacademy.com>
2015-04-25 23:51:15 -0700

    testing some file movement

--- application.py
+++ application.py
@@ -1,7 +1,22 @@
-from flask import flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+@app.route("/")
+def start_here():
+    return "Hi! this is the start of the application for Ubermelon"
 
 
-if __name__ == "__main__"
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
