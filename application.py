5bcb92f911f56cfbcab2fda5568ff08d5e61f9f6
Hackbright Student <info@hackbrightacademy.com>
2015-04-26 15:09:25 -0700

    commiting routed applicationg and rendered HTML

--- application.py
+++ application.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def start_here():
-    return "Hi! this is the start of the application for Ubermelon"
+     app = render_template("application-form.html")
+     print app
+     return app
+#this step renders the HTML from the file onto the server site
+#
 
-@app.route("/uberapplicationstart")
-def uber_applicationstart():
+
 
 if __name__ == "__main__":
     app.run(debug=True)
